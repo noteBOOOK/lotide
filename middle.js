d49ee41f2a4241fd659ab2fa5347b4924c73d256
Jason Long <jason.long77@hotmail.com>
2020-09-16 17:40:33 +0000

    Created middle function to get the middle value of an array

---
+++ middle.js
@@ -0,0 +1,42 @@
+const eqArrays = function(firstArray, secondArray) {
+  if (firstArray.length !== secondArray.length) {
+    return false;
+  }
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(firstArray, secondArray) {
+  if (eqArrays(firstArray, secondArray)) {
+    console.log(`✅✅✅ Assertion Passed: "${firstArray}" === "${secondArray}"`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: "${firstArray}" !== "${secondArray}"`);
+  }
+};
+
+const middle = function(listOfNumbers) {
+  let middleNums = [];
+  const midValue = listOfNumbers.length / 2;
+  if (listOfNumbers.length <=2) {
+    return middleNums;
+  } else {
+    if (listOfNumbers.length % 2 !== 0) {
+      middleNums.push(listOfNumbers[Math.floor(midValue)]);
+      return middleNums;
+    } else {
+      middleNums.push(listOfNumbers[(midValue) - 1]);
+      middleNums.push(listOfNumbers[midValue]);
+      return middleNums;
+    }
+  }
+};
+
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3]), [1, 2, 3]);
+assertArraysEqual(middle([1, 2]), []);
