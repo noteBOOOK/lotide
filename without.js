ebded8f5bf5dd8f729a3c0d061bb16bc582bfcb7
Jason Long <jason.long77@hotmail.com>
2020-09-16 00:46:50 +0000

    Created a Without function removing unwanted values from an array

---
+++ without.js
@@ -0,0 +1,35 @@
+const assertArraysEqual = function(firstArray, secondArray) {
+  if (eqArrays(firstArray, secondArray)) {
+    console.log(`✅✅✅ Assertion Passed: Both arrays are identical!`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: The arrays are different!`);
+  }
+};
+
+const eqArrays = function(firstArray, secondArray) {
+  if (firstArray.length !== secondArray.length) {
+    return false;
+  }
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const without = function (firstArray, secondArray) {
+  let output = [];
+  for (let i = 0; i < firstArray.length; i++) {
+    let current = firstArray[i];
+    if (!secondArray.includes(current)) {
+      output.push(current);
+      console.log(current);
+    }
+  }
+  console.log(output);
+  return output;
+};
+
+without([1, 2, 3], [1]) // => [2, 3]
+without(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
