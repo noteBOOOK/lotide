5c58eb83715f19c81d1c3306e8dcc8d948ef4c10
Jason Long <jason.long77@hotmail.com>
2020-09-16 19:56:15 +0000

    ESLint on the file

--- countOnly.js
+++ countOnly.js
@@ -8,9 +8,8 @@ const assertEqual = function(actual, expected) {
 
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
-
   for (const item of allItems) {
-    if (itemsToCount[item])  
+    if (itemsToCount[item])
       if (results[item]) {
         results[item] += 1;
       } else {
