4ec0a68a2b774ca993f203726e3c37b813eb1494
Jason Long <jason.long77@hotmail.com>
2020-09-21 19:37:48 +0000

    Added Mocha and Chai tests

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,30 @@
 const middle = require("../middle");
 const assertArraysEqual = require("../assertArraysEqual");
+const assert = require("chai").assert;
+const expect = require("chai").expect;
 
+describe("#middle", () => {
+  it("Should return [2] for [1, 2, 3]", () => {
+    let input = middle([1, 2, 3]);
+    let expectedOutput = [2];
+    expect(input).to.eql(expectedOutput);
+  });
 
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2]), []);
+  it("Should return [2, 3] for [1, 2, 3, 4]", () => {
+    let input = middle([1, 2, 3, 4]);
+    let expectedOutput = [2, 3];
+    expect(input).to.eql(expectedOutput);
+  });
+
+  it("Should return [3] for [1, 2, 3, 4, 5]", () => {
+    let input = middle([1, 2, 3, 4, 5]);
+    let expectedOutput = [3];
+    expect(input).to.eql(expectedOutput);
+  });
+
+  it("Should return [] for [1, 2]", () => {
+    let input = middle([1, 2]);
+    let expectedOutput = [];
+    expect(input).to.eql(expectedOutput);
+  });
+});
