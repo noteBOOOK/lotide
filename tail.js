58b851127c00ca5cd4360858d231c15267e8e44e
Jason Long <jason.long77@hotmail.com>
2020-09-21 19:48:06 +0000

    Removed tests

--- tail.js
+++ tail.js
@@ -12,8 +12,3 @@ const tail = function(array) {
 
 
 module.exports = tail;
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
