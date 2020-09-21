b1d6681060ebde20f57ed8556fb4a5f22db0b257
Jason Long <jason.long77@hotmail.com>
2020-09-21 19:52:51 +0000

    Exported the eqObjects function

--- eqObjects.js
+++ eqObjects.js
@@ -1,22 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: "${actual}" === "${expected}"`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: "${actual}" !== "${expected}"`);
-  }
-};
-
-const eqArrays = function(firstArray, secondArray) {
-  if (firstArray.length !== secondArray.length) {
-    return false;
-  }
-  for (let i = 0; i < firstArray.length; i++) {
-    if (firstArray[i] !== secondArray[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require("./eqArrays");
 
 const eqObjects = function(object1, object2) {
   let firstKeys = Object.keys(object1);
@@ -37,20 +19,22 @@ const eqObjects = function(object1, object2) {
   }
 };
 
-
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-assertEqual(eqObjects(ab, ba), true);
-//console.log(eqObjects(ab, ba));
-const abc = { a: "1", b: "2", c: "3" };
-assertEqual(eqObjects(ab, abc), false);
-//console.log(eqObjects(ab, abc));
+module.exports = eqObjects;
 
 
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-assertEqual(eqObjects(cd, dc), true);
-//console.log(eqObjects(cd, dc));
-const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2), false);
-//console.log(eqObjects(cd, cd2));
+// const ab = { a: "1", b: "2" };
+// const ba = { b: "2", a: "1" };
+// assertEqual(eqObjects(ab, ba), true);
+// //console.log(eqObjects(ab, ba));
+// const abc = { a: "1", b: "2", c: "3" };
+// assertEqual(eqObjects(ab, abc), false);
+// //console.log(eqObjects(ab, abc));
+
+
+// const cd = { c: "1", d: ["2", 3] };
+// const dc = { d: ["2", 3], c: "1" };
+// assertEqual(eqObjects(cd, dc), true);
+// //console.log(eqObjects(cd, dc));
+// const cd2 = { c: "1", d: ["2", 3, 4] };
+// assertEqual(eqObjects(cd, cd2), false);
+// //console.log(eqObjects(cd, cd2));
