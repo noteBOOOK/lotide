40d6100f490b9bbe76ce1e6b191058aef46a10a3
Jason Long <jason.long77@hotmail.com>
2020-09-21 17:09:26 +0000

    Exported tail function and moved tests to test file

--- tail.js
+++ tail.js
@@ -10,6 +10,9 @@ const tail = function(array) {
   return array.slice(1);
 };
 
+
+module.exports = tail;
+
 const result = tail(["Hello", "Lighthouse", "Labs"]);
 assertEqual(result.length, 2); // ensure we get back two elements
 assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
