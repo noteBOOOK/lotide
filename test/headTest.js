2b61cc6a76166c837c714b4e9e6cc3bf4fd30fa4
Jason Long <jason.long77@hotmail.com>
2020-09-21 18:52:44 +0000

    Debugged tests

--- test/headTest.js
+++ test/headTest.js
@@ -3,7 +3,7 @@ const assert = require("chai").assert;
 
 describe("#head", () => {
   it("Should return 1 for [1, 2, 3]", () => {
-    assert.strictEqual(head[1, 2, 3]);
+    assert.strictEqual(head([1, 2, 3]), 1);
   });
   it("Should return '5' for ['5']", () => {
     assert.strictEqual(head(['5']), '5');
