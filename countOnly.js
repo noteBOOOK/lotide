22ae0da5da12647a57cdf49e8ee4f37a106ee9c9
Jason Long <jason.long77@hotmail.com>
2020-09-21 20:35:21 +0000

    Commented out assertEqual

--- countOnly.js
+++ countOnly.js
@@ -1,10 +1,10 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: "${actual}" === "${expected}"`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: "${actual}" !== "${expected}"`);
-  }
-};
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`✅✅✅ Assertion Passed: "${actual}" === "${expected}"`);
+//   } else {
+//     console.log(`🛑🛑🛑 Assertion Failed: "${actual}" !== "${expected}"`);
+//   }
+// };
 
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
