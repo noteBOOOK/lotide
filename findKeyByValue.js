683226a454c20c7f8507db77e6873b21fe618b44
Jason Long <jason.long77@hotmail.com>
2020-09-21 20:42:30 +0000

    Commented out assertEqual

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,10 +1,10 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: "${actual}" === "${expected}"`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: "${actual}" !== "${expected}"`);
-  }
-};
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`✅✅✅ Assertion Passed: "${actual}" === "${expected}"`);
+//   } else {
+//     console.log(`🛑🛑🛑 Assertion Failed: "${actual}" !== "${expected}"`);
+//   }
+// };
 
 const findKeyByValue = function(objectList, value) {
   for (let key in objectList) {
